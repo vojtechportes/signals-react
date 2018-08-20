352d3252327cb337b7e7aa7eccc0050b332819c6
Vojtech Portes <mdouglas64@gmail.com>
2018-08-20 04:52:51 +0200

    CSS formating fix

--- src/js/view/components/Navigation/Navigation.js
+++ src/js/view/components/Navigation/Navigation.js
@@ -27,8 +27,8 @@ const Container = styled.nav`
 	height: 100%;		
 	background-image: linear-gradient(0deg, ${props => props.theme.colors.primaryDark} 0%, ${props => props.theme.colors.primaryLight} 100%);
     background-size: auto 900px;
-    background-repeat: no-repeat;
-    background-color: ${props => props.theme.colors.primaryDark};	
+	background-repeat: no-repeat;
+	background-color: ${props => props.theme.colors.primaryDark};	
 	color: ${props => props.theme.colors.light};
 
 	${props => props.opened && css`
